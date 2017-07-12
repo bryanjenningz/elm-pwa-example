9a08143974c3170b9efb32b2061941197043c20f
Bryan Jennings <bryanjenningz@gmail.com>
2017-07-12 16:53:41 -0700

    remove server mock data

--- server.js
+++ server.js
@@ -9,15 +9,4 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("dist"));
 app.use("/api", require("./api/routes/index"));
-
-// Keeping this here for testing
-// const { mockUser, mockMoments } = require("./mockData");
-// app.get("/user", (req, res) => res.status(200).json(mockUser));
-// app.get("/moments", (req, res) => res.status(200).json(mockMoments));
-// app.get("/search", (req, res) =>
-//   res.status(200).json(Array.from({ length: 10 }, () => mockUser))
-// );
-// app.post("/login", (req, res) => res.status(200).json(mockUser));
-// app.post("/signup", (req, res) => res.status(200).json(mockUser));
-
 app.listen(3000, () => console.log("Listening on port 3000"));
